6eaac49f2eaab5743486a9e6821d190902f7dfe4
dolovnyak <dolovnyak@yandex.ru>
2022-01-20 01:59:43 +0300

    change commit

--- rubik_solver/heuristic_databases/thistlethwaite/TetradPairedPlus4EdgePermutationsDB.hpp
+++ rubik_solver/heuristic_databases/thistlethwaite/TetradPairedPlus4EdgePermutationsDB.hpp
@@ -14,7 +14,7 @@
 /// The parity is also stored.
 /// Parity of corners and edges is related - when the edge parity is even, the corner parity is even too.
 /// This doubles the number of elements the last bit in the index is used for parity, 0 for even, 1 for odd.
-/// 70 * 2520 * 2 / 1024^2 / 2 = 352800 / 1024^2 / 2 = ~.17MB on disk.
+/// 70 * 2520 * 2 / 1024^2 / 2 = 352800 / 1024^2 / 2 = ~168 kb on disk.
 static constexpr size_t kThistlethwaiteStep3DbSize = 352800;
 
 static constexpr std::array<Cube::Rotation, 10> kThistlethwaiteStep3Rotations = {
