1caeaf4324e07933b1594c2b14700aceb25598cf
dolovnyak <dolovnyak@yandex.ru>
2022-01-19 17:31:00 +0300

    it works

--- rubik_solver/solvers/find_solve_through_ida_star.hpp
+++ rubik_solver/solvers/find_solve_through_ida_star.hpp
@@ -30,20 +30,21 @@ struct PriorityNode {
 template<size_t MaxRotationsSize, class Heuristic, size_t RotationsSize>
 std::vector<Cube::Rotation> find_solve_through_ida_star(
         Heuristic heuristic, const Cube& cube, std::array<Cube::Rotation, RotationsSize> possible_rotations) {
-    std::queue<StateNode> nodes;
-    std::priority_queue<PriorityNode, std::vector<PriorityNode>, std::greater<>> tmp_nodes;
+    std::stack<StateNode> nodes;
+    std::queue<PriorityNode> tmp_nodes;
     std::array<Cube::Rotation, MaxRotationsSize> rotations;
 
-    uint8_t current_optimal_estimate = heuristic.GetEstimatedMovesNumber(cube);
+    uint8_t current_optimal_estimate = heuristic.GetEstimatedMovesNumber(cube) - 1;
     uint8_t max_depth = 0;
 
+    bool is_solved = false;
     while (true) {
         if (nodes.empty()) {
             nodes.emplace(cube, 0, Cube::Rotation::None);
             ++current_optimal_estimate;
         }
 
-        StateNode current_node = nodes.front();
+        StateNode current_node = nodes.top();
         nodes.pop();
         std::cout << (int) current_optimal_estimate << std::endl;
 
@@ -66,31 +67,31 @@ std::vector<Cube::Rotation> find_solve_through_ida_star(
         }
 
         while (!tmp_nodes.empty()) {
-            nodes.emplace(tmp_nodes.top().cube, current_node.depth + 1, tmp_nodes.top().rotation);
+            nodes.emplace(tmp_nodes.front().cube, current_node.depth + 1, tmp_nodes.front().rotation);
             tmp_nodes.pop();
         }
 
 
                 /////////////////////////////////
-//                std::cout << "AAAA" << std::endl;
-//                if (current_node.rotation == Cube::Rotation::None) {
-//                    std::cout << "rotation: None" << std::endl;
-//                }
-//                else {
-//                    std::vector<Cube::Rotation> rot {current_node.rotation};
-//                    std::cout << "rotation: " << rot << std::endl;
-//                }
-//                print_cube(current_node.cube);
-//                std::cout << std::endl;
-//                Cube check(cube);
-//                std::vector<Cube::Rotation> res_rotations;
-//                for (uint8_t i = 0; i <= max_depth; ++i) {
-//                    if (rotations[i] != Cube::Rotation::None) {
-//                        res_rotations.push_back(rotations[i]);
-//                    }
-//                }
-//                print_cube(check.ApplyRotations(res_rotations));
-//                std::cout << "BBBB" << std::endl;
+                std::cout << "AAAA" << std::endl;
+                if (current_node.rotation == Cube::Rotation::None) {
+                    std::cout << "rotation: None" << std::endl;
+                }
+                else {
+                    std::vector<Cube::Rotation> rot {current_node.rotation};
+                    std::cout << "rotation: " << rot << std::endl;
+                }
+                print_cube(current_node.cube);
+                std::cout << std::endl;
+                Cube check(cube);
+                std::vector<Cube::Rotation> res_rotations;
+                for (uint8_t i = 0; i <= max_depth; ++i) {
+                    if (rotations[i] != Cube::Rotation::None) {
+                        res_rotations.push_back(rotations[i]);
+                    }
+                }
+                print_cube(check.ApplyRotations(res_rotations));
+                std::cout << "BBBB" << std::endl;
         /////////////////////////////////////////
     }
 
